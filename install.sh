e7664cce9b56cc9a2c77fe67d53165321679226c
Emilien-Foissotte <emilien.foissotte@sita.aero>
2023-06-09 12:14:20 +0200

    added more explanation to install and tmux loading

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ set -euo pipefail
 # sudo apt-get update && sudo apt-get install tmux neovim
 
 cp ./.tmux.conf ~
+tmux source ~/.tmux.conf
 mkdir -p ~/.config/nvim
 cp -r .config/nvim ~/.config/nvim
+
+# start neovim, packer will tell you it has install plugins
+# then start Mason, :Mason to install all lintings, lsp servers etc..
