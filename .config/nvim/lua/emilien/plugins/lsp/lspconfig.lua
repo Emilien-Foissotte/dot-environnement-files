f46977ddff830e7eddaa98c307345b99556ea5ae
Émilien Foissotte <emilienfoissotte44@gmail.com>
2023-06-09 14:50:49 +0200

    fixed on attach name

--- .config/nvim/lua/emilien/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/emilien/plugins/lsp/lspconfig.lua
@@ -30,6 +30,10 @@ local on_attach = function(client, bufnr)
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+  --specific keybinds per client
+  if client.name == "jedi_language_server" then
+  end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
