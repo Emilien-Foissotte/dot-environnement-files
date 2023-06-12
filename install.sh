700601692557314e730b36bc1ba469e06d0562c3
Emilien-Foissotte <emilien.foissotte@sita.aero>
2023-06-12 14:50:06 +0200

    added hadolint as Docker linting tool

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 # be sure to have install neovim first and tmux
 # sudo apt-get update && sudo apt-get install tmux neovim
+
 # clone tpm plugin
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -26,5 +27,11 @@ mkdir -p ~/.config/nvim
 cp -r .config/nvim ~/.config/
 
 # install nodejs npm and default-jre for mason lsp tools
+# sudo apt-get update & sudo apt-get install nodejs npm default-jre
+
+# install hadolint for Docker linting
+# sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-(uname -m)
+# sudo sudo chmod +x /bin/hadolint
+
 # start neovim, packer will tell you it has install plugins
 # then start Mason, :Mason to install all lintings, lsp servers etc..
